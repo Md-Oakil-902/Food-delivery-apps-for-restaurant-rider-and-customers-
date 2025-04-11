e53a93b2624a3570053d8203d4e7c5fa1d777216
mdoakil902 <159051140+Mdoakil@users.noreply.github.com>
2025-04-11 22:14:39 +0600

    Bouncy Splash screen implementation

---
+++ app/src/main/java/com/oakil/fooddeliveryapplication/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.oakil.fooddeliveryapplication
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.addListener
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.oakil.fooddeliveryapplication.ui.theme.FoodDeliveryApplicationTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    var showSplashScreen = true
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                showSplashScreen
+            }
+     setOnExitAnimationListener{
+                screen->
+                val zoomx = ObjectAnimator.ofFloat(screen.iconView, View.SCALE_X, 0.5f, 0f)
+                val zoomY = ObjectAnimator.ofFloat(screen.iconView, View.SCALE_Y, 0.5f, 0f)
+
+                val animatorSet = AnimatorSet().apply {
+                    playTogether(zoomx, zoomY)
+                    duration = 500
+                    interpolator = OvershootInterpolator()
+                    addListener(onEnd = {
+                        screen.remove()
+                    })
+
+                }
+                animatorSet.start()
+
+            }
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FoodDeliveryApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Oakil",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(3000)
+            showSplashScreen = false
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FoodDeliveryApplicationTheme {
+        Greeting("Android")
+    }
+}}
