381c2053cd92951d84e0dc208c563759035af3a4
mdoakil902 <159051140+Mdoakil@users.noreply.github.com>
2025-04-15 22:22:35 +0600

    started working on Authscreeen welcome screen

---
+++ app/src/main/java/com/oakil/fooddeliveryapplication/ui/features/home/AuthScreen.kt
@@ -0,0 +1,9 @@
+package com.oakil.fooddeliveryapplication.ui.features.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AuthScreen(modifier: Modifier = Modifier) {
+    
+}
