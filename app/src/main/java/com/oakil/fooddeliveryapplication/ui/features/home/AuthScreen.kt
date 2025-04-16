7e88fb00036e12ca24b8ee4e2f784b16111b7b1e
mdoakil902 <159051140+Mdoakil@users.noreply.github.com>
2025-04-16 23:26:53 +0600

    brushing and made image size dynamic

--- app/src/main/java/com/oakil/fooddeliveryapplication/ui/features/home/AuthScreen.kt
+++ app/src/main/java/com/oakil/fooddeliveryapplication/ui/features/home/AuthScreen.kt
@@ -1,10 +1,50 @@
 package com.oakil.fooddeliveryapplication.ui.features.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
+import com.oakil.fooddeliveryapplication.R
 
 @Composable
-fun AuthScreen(modifier: Modifier = Modifier) {
-    
-    
+fun AuthScreen() {
+    val imageSize = remember {
+        mutableStateOf(IntSize.Zero)
+    }
+
+    val brush = Brush.verticalGradient(
+        colors = listOf(
+            Color.Transparent, Color.Black
+        ),
+        startY = imageSize.value.height.toFloat()/3
+    )
+
+    Box(modifier = Modifier.fillMaxSize().background(Color.Black)) {
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = null,
+            modifier = Modifier.onGloballyPositioned {
+                imageSize.value = it.size
+            }.alpha(0.6f))
+        Box(modifier = Modifier.matchParentSize().background(brush = brush))
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun AuthScreenPreview() {
+    AuthScreen()
 }
