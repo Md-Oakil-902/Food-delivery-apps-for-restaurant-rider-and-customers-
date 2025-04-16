6e8d4f9f44b4768f5c6ef2157e3b2c4e6fd26542
mdoakil902 <159051140+Mdoakil@users.noreply.github.com>
2025-04-16 23:36:19 +0600

    adding skip button

--- app/src/main/java/com/oakil/fooddeliveryapplication/ui/features/home/AuthScreen.kt
+++ app/src/main/java/com/oakil/fooddeliveryapplication/ui/features/home/AuthScreen.kt
@@ -4,18 +4,26 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import com.oakil.fooddeliveryapplication.R
+import com.oakil.fooddeliveryapplication.ui.theme.Orange
 
 @Composable
 fun AuthScreen() {
@@ -27,17 +35,41 @@ fun AuthScreen() {
         colors = listOf(
             Color.Transparent, Color.Black
         ),
-        startY = imageSize.value.height.toFloat()/3
+        startY = imageSize.value.height.toFloat() / 3
     )
 
-    Box(modifier = Modifier.fillMaxSize().background(Color.Black)) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
         Image(
             painter = painterResource(id = R.drawable.background),
             contentDescription = null,
-            modifier = Modifier.onGloballyPositioned {
-                imageSize.value = it.size
-            }.alpha(0.6f))
-        Box(modifier = Modifier.matchParentSize().background(brush = brush))
+            modifier = Modifier
+                .onGloballyPositioned {
+                    imageSize.value = it.size
+                }
+                .alpha(0.6f)
+        )
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .background(brush = brush)
+        )
+
+
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+            modifier = Modifier.align(
+                Alignment.TopEnd
+            ).padding(8.dp)
+        ) {
+            Text(text = stringResource(R.string.skip), color = Orange)
+        }
+
+
     }
 
 }
