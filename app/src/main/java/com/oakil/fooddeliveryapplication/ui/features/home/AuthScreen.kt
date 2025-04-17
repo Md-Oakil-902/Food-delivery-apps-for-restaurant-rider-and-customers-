6bda17c2c215e9ab9ddfeca3936ab62c67543cae
mdoakil902 <159051140+Mdoakil@users.noreply.github.com>
2025-04-17 23:58:57 +0600

    auth screen design done

--- app/src/main/java/com/oakil/fooddeliveryapplication/ui/features/home/AuthScreen.kt
+++ app/src/main/java/com/oakil/fooddeliveryapplication/ui/features/home/AuthScreen.kt
@@ -1,15 +1,21 @@
 package com.oakil.fooddeliveryapplication.ui.features.home
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,6 +34,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.oakil.fooddeliveryapplication.R
 import com.oakil.fooddeliveryapplication.ui.theme.Orange
+import com.oakil.fooddeliveryapplication.ui.theme.White
 
 @Composable
 fun AuthScreen() {
@@ -99,7 +106,71 @@ fun AuthScreen() {
                 fontSize = 16.sp,
                 modifier = Modifier.padding(vertical = 16.dp),
 
-            )
+                )
+
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(containerColor = White),
+                    shape = RoundedCornerShape(32.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.padding(start = 8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.ic_facebook),
+                            contentDescription = "google",
+                            modifier = Modifier.size(24.dp)
+                        )
+                        Spacer(modifier = Modifier.size(8.dp))
+                        Text(text = stringResource(R.string.facebook), color = Color.Black)
+                    }
+                }
+
+            Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(containerColor = White),
+                    shape = RoundedCornerShape(32.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.padding(start = 8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.ic_google),
+                            contentDescription = "google",
+                            modifier = Modifier.size(24.dp)
+                        )
+                        Spacer(modifier = Modifier.size(8.dp))
+                        Text(text = stringResource(R.string.Google), color = Color.Black)
+                    }
+                }
+
+
+            }
+
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Gray.copy(alpha = 0.2f)),
+                shape = RoundedCornerShape(32.dp),
+                border = BorderStroke(1.dp, color = Color.White)
+            ) {
+                Text(text = stringResource(R.string.sign_with_phone), color = White)
+            }
+
+            TextButton(onClick = {}, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                Text(text = stringResource(id = R.string.already_have_account), color = Color.White)
+            }
 
         }
 
