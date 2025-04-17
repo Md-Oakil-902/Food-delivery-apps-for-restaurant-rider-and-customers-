ffad91bc75491af290928673deecb1498036426e
mdoakil902 <159051140+Mdoakil@users.noreply.github.com>
2025-04-17 23:21:23 +0600

    working on authscreen

--- app/src/main/java/com/oakil/fooddeliveryapplication/ui/features/home/AuthScreen.kt
+++ app/src/main/java/com/oakil/fooddeliveryapplication/ui/features/home/AuthScreen.kt
@@ -3,7 +3,9 @@ package com.oakil.fooddeliveryapplication.ui.features.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -19,9 +21,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.oakil.fooddeliveryapplication.R
 import com.oakil.fooddeliveryapplication.ui.theme.Orange
 
@@ -58,15 +62,45 @@ fun AuthScreen() {
                 .background(brush = brush)
         )
 
-
         Button(
             onClick = {},
             colors = ButtonDefaults.buttonColors(containerColor = Color.White),
-            modifier = Modifier.align(
-                Alignment.TopEnd
-            ).padding(8.dp)
+            modifier = Modifier
+                .align(
+                    Alignment.TopEnd
+                )
+                .padding(8.dp)
         ) {
             Text(text = stringResource(R.string.skip), color = Orange)
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 110.dp, start = 16.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.welcome),
+                color = Color.Black,
+                fontSize = 50.sp,
+                modifier = Modifier,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = stringResource(R.string.app_name),
+                color = Orange,
+                fontSize = 50.sp,
+                modifier = Modifier,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = stringResource(R.string.foodhub_description),
+                color = Color.DarkGray,
+                fontSize = 16.sp,
+                modifier = Modifier.padding(vertical = 16.dp),
+
+            )
+
         }
 
 
